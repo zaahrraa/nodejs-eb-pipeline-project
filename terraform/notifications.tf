096442c378a50f31ce34eed5c80bfeb70e9446f1
zaahrraa <syedazahramahmood@gmail.com>
2026-09-01 23:41:14 +0500

    add screenshots

--- terraform/notifications.tf
+++ terraform/notifications.tf
@@ -5,7 +5,7 @@ resource "aws_sns_topic" "pipeline_notifications" {
 resource "aws_sns_topic_subscription" "email" {
   topic_arn = aws_sns_topic.pipeline_notifications.arn
   protocol  = "email"
-  endpoint  = "syedazahramahmood@example.com"   
+  endpoint  = "syedazahramahmood@gmail.com"   
 }
 
 resource "aws_codestarnotifications_notification_rule" "pipeline_notify" {
